1aa941a3ca517781984f76cf4e73e5d07be6fcd0
chinnulakku4 <chinnulakku4@gmail.com>
2026-04-29 17:25:06 +0530

    robo-shell

--- catalogue.sh
+++ catalogue.sh
@@ -20,9 +20,72 @@ VALIDATE() {
     fi
 }
 
-if($? -ne 0) {
+if [ $? -ne 0 ]
 then
     echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1
 else
-    echo "
-}
+    echo "you are root user"
+fi
+
+dnf module disable nodejs -y &>> $LOGFILE
+
+VALIDATE $? " Disabling current NodeJS" 
+
+dnf module enable nodejs:18 -y &>> $LOGFILE
+
+VALIDATE $? "Enabling nodejs:18"
+
+dnf install nodejs -y &>> $LOGFILE
+
+VALIDATE $? " Installing nodejs:18"
+
+useradd roboshop &>> $LOGFILE
+
+VALIDATE $? " creating roboshop user" 
+
+mkdir /app &>> $LOGFILE
+
+VALIDATE $? "creating app directory" 
+
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>> $LOGFILE
+
+VALIDATE $? "Downloading catalogue application"
+
+cd /app &>> $LOGFILE
+
+unzip /tmp/catalogue.zip
+
+VALIDATE $? "unzipping catalogue" 
+
+npm install &>> $LOGFILE
+
+VALIDATE $?" Installing dependencies" &>> $LOGFILE
+
+cp /home/centos/robo-shell/catalogue.service /etc/systemd/system/catalogue.service &>> $LOGFILE
+
+VALIDATE $? "copying catalogue service"
+
+systemctl daemon-reload &>> $LOGFILE
+
+VALIDATE $? "catalogue daemon reload" 
+
+systemctl enable catalogue &>> $LOGFILE
+
+VALIDATE $? " Enable catalogue"
+
+systemctl start catalogue &>> $LOGFILE
+
+VALIDATE $? "start catalogue"
+
+cp /home/centos/robo-shell/mongodb.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+
+VALIDATE $? "copying mongodb repo"
+
+dnf install mongodb-org-shell -y &>> $LOGFILE
+
+VALIDATE $? " Installing MONGODB client"
+
+mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js &>> $LOGFILE
+
+VALIDATE $? "Loading catalogue data into MongoDB"
