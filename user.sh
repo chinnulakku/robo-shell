9935223281bfdaa7af0978a0cc5ba1b03cf5696f
chinnulakku4 <chinnulakku4@gmail.com>
2026-05-02 16:58:27 +0530

    robo-shell

--- user.sh
+++ user.sh
@@ -13,7 +13,7 @@ LOGFILE=/tmp/$0-$TIMESTAMP.log
 echo "script starts executing with $TIMESTAMP" &>> $LOGFILE
 
 VALIDATE(){
-    if[ $1 -ne 0 ]
+    if [ $1 -ne 0 ]
     then
         echo -e "$2...$R FAILED $N"
     else
